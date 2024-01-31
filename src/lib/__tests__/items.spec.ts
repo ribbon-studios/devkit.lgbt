4cd62aadbcda546c50cc2522b5335af906421b73
Cecilia Sanare <admin@cecilias.me>
2024-01-31 00:39:06 -0600

    fix: properly count the number of items

--- src/lib/__tests__/items.spec.ts
+++ src/lib/__tests__/items.spec.ts
@@ -1,6 +1,6 @@
 import { Todo } from '@/storage';
 import { describe, expect, it } from 'vitest';
-import { isDone, setDone } from '../items';
+import { getStatusCount, isDone, setDone } from '../items';
 
 describe('Item Utils', () => {
   describe('fn(isDone)', () => {
@@ -112,6 +112,125 @@ describe('Item Utils', () => {
           done: true,
         },
       ]);
+    });
+  });
+
+  describe('fn(getStatusCount)', () => {
+    it('should support a simple item being done', () => {
+      expect(
+        getStatusCount({
+          id: '',
+          label: '',
+          done: true,
+          subItems: [],
+        })
+      ).toEqual({
+        done: 1,
+        total: 1,
+      });
+    });
+
+    it('should support a simple item not being done', () => {
+      expect(
+        getStatusCount({
+          id: '',
+          label: '',
+          done: false,
+          subItems: [],
+        })
+      ).toEqual({
+        done: 0,
+        total: 1,
+      });
+    });
+
+    it('should support nested items', () => {
+      expect(
+        getStatusCount({
+          id: '',
+          label: '',
+          done: true,
+          subItems: [
+            {
+              id: '',
+              label: '',
+              done: true,
+              subItems: [],
+            },
+            {
+              id: '',
+              label: '',
+              done: true,
+              subItems: [],
+            },
+          ],
+        })
+      ).toEqual({
+        done: 2,
+        total: 2,
+      });
+    });
+
+    it('should support partially completed nested items', () => {
+      expect(
+        getStatusCount({
+          id: '',
+          label: '',
+          done: false,
+          subItems: [
+            {
+              id: '',
+              label: '',
+              done: false,
+              subItems: [],
+            },
+            {
+              id: '',
+              label: '',
+              done: true,
+              subItems: [],
+            },
+          ],
+        })
+      ).toEqual({
+        done: 1,
+        total: 2,
+      });
+    });
+
+    it('should support complex lists', () => {
+      expect(
+        getStatusCount([
+          {
+            id: '',
+            label: '',
+            done: true,
+            subItems: [
+              {
+                id: '',
+                label: '',
+                done: true,
+                subItems: [],
+              },
+              {
+                id: '',
+                label: '',
+                done: true,
+                subItems: [],
+              },
+            ],
+          },
+          {
+            id: '',
+            label: '',
+            done: true,
+            subItems: [],
+          },
+        ])
+      ).toEqual({
+        done: 3,
+        total: 3,
+      });
     });
   });
 });
--- src/lib/items.ts
+++ src/lib/items.ts
@@ -35,3 +35,64 @@ export function hasChanged(updatedItem: Todo.Item, originalItem?: Todo.Item): bo
 export function hasNotChanged(updatedItem: Todo.Item, originalItem?: Todo.Item): boolean {
   return !hasChanged(updatedItem, originalItem);
 }
+
+export type StatusCountResult = {
+  done: number;
+  total: number;
+};
+
+// TODO(Refactor): Likely a way cleaner way to do this that my brain just isn't coming up with at the moment
+export function getStatusCount(items: Todo.Item | Todo.Item[]): StatusCountResult {
+  if (items === undefined)
+    return {
+      done: 0,
+      total: 0,
+    };
+
+  if (Array.isArray(items)) {
+    return items.reduce(
+      (output, item) => {
+        const count = getStatusCount(item);
+
+        return {
+          done: output.done + count.done,
+          total: output.total + count.total,
+        };
+      },
+      {
+        done: 0,
+        total: 0,
+      }
+    );
+  }
+
+  if (items.subItems.length > 0) {
+    return getStatusCount(items.subItems);
+  }
+
+  if (!items.done) {
+    return {
+      done: 0,
+      total: 1,
+    };
+  }
+
+  return {
+    done: 1,
+    total: 1,
+  };
+}
+
+export enum ListStatus {
+  NOT_STARTED,
+  IN_PROGRESS,
+  DONE,
+}
+
+export function getStatus({ done, total }: StatusCountResult): ListStatus {
+  if (done === 0) return ListStatus.NOT_STARTED;
+
+  if (done === total) return ListStatus.DONE;
+
+  return ListStatus.IN_PROGRESS;
+}
